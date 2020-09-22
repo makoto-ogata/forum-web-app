6a81818797e27053b7e07da2b032850197076fc6
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-09-23 02:33:11 +0900

    finished section 2-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import Header from './components/Header';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PageHome from './components/PageHome';
+import PageThread from './components/PageThread';
+import PageNotFound from './components/PageNotFound';
 
-function App() {
+const App =()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+    <Switch>
+      <Route exact path={'/'} >
+        <PageHome />
+      </Route>
+      <Route exact path={'/threads/:threadId'} >
+        <PageThread />
+      </Route>
+      <Route exact path={'*'} >
+        <PageNotFound />
+      </Route>
+    </Switch>
+   </BrowserRouter> 
   );
 }
 
