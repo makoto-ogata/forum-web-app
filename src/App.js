a389c516c2d403613101848471d4bb355cd54aa9
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-09-23 02:46:59 +0900

    finished section 2-3

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { createMuiTheme, CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 //import Header from './components/Header';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
@@ -6,20 +8,25 @@ import PageThread from './components/PageThread';
 import PageNotFound from './components/PageNotFound';
 
 const App =()=> {
+  const theme = createMuiTheme();
+
   return (
-   <BrowserRouter>
-    <Switch>
-      <Route exact path={'/'} >
-        <PageHome />
-      </Route>
-      <Route exact path={'/threads/:threadId'} >
-        <PageThread />
-      </Route>
-      <Route exact path={'*'} >
-        <PageNotFound />
-      </Route>
-    </Switch>
-   </BrowserRouter> 
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path={'/'} >
+            <PageHome />
+          </Route>
+          <Route exact path={'/threads/:threadId'} >
+            <PageThread />
+          </Route>
+          <Route exact path={'*'} >
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
