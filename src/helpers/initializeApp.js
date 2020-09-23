21f22e85dc01535c45469ad5d7d7ae730c3bd83f
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-09-23 23:53:26 +0900

    finished section 3-1

---
+++ src/helpers/initializeApp.js
@@ -0,0 +1,13 @@
+import { firestore, initializeApp } from 'firebase/app';
+import 'firebase/firestore';
+
+initializeApp({
+  apiKey: "AIzaSyBrCU72PXCILKJIyfCDt0ynl0em9R2Jgo0",
+  authDomain: "forum-web-app-5a575.firebaseapp.com",
+  databaseURL: "https://forum-web-app-5a575.firebaseio.com",
+  projectId: "forum-web-app-5a575",
+  storageBucket: "forum-web-app-5a575.appspot.com",
+  messagingSenderId: "568915174359",
+  appId: "1:568915174359:web:4906b5eea4df5165ede192",
+  measurementId: "G-SERC59BZTK"
+})
