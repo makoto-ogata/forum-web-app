2e75acfe1c29dee365bdb1b26f5a1f21312820c0
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-09-24 10:34:55 +0900

    finished section 3-3

--- src/components/PageHome.js
+++ src/components/PageHome.js
@@ -1,10 +1,13 @@
+import { firestore } from 'firebase/app';
 import React from 'react';
+import FormThread from './FormThread';
 import Main from './Main';
 
 const PageHome = () => {
   return (
     <Main>
       <h1>{'Home'}</h1>
+      <FormThread />
     </Main>
   )
 }
