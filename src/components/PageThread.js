1622c5d65cf66ca9965ae38d583002fa9405b1fc
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-09-25 22:43:39 +0900

    finished section 4-2

--- src/components/PageThread.js
+++ src/components/PageThread.js
@@ -1,10 +1,16 @@
+import {firestore} from 'firebase/app'
 import React from 'react';
+import {useParams} from 'react-router-dom';
+import FormResponse from './FormResponse';
 import Main from './Main';
 
 const PageThread =()=> {
+  const {threadId} = useParams();
+
   return (
     <Main>
       <h1>{'Thread'}</h1>
+      <FormResponse threadId={threadId} />
     </Main>
   )
 }
